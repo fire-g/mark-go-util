0bcfed743aaa002e93ba48867face18be7153c50
fire-g <18296517259@163.com>
2020-12-31 00:11:01 +0800

    update md5 func

--- md5.go
+++ md5.go
@@ -1,16 +1,41 @@
 package util
 
 import (
+	"bytes"
 	"crypto/md5"
+	"encoding/binary"
 	"fmt"
 )
 
-func Md5String(str string) string {
-	md5str := fmt.Sprintf("%x", Md5Bytes(str))
+func StringToMd5String(str string) string {
+	md5str := fmt.Sprintf("%x", StringToMd5Bytes(str))
 	return md5str
 }
 
-func Md5Bytes(str string) [16]byte {
+func StringToMd5Bytes(str string) [16]byte {
 	data := []byte(str)
 	return md5.Sum(data)
+}
+
+func IntToMd5Bytes(i int) [16]byte {
+	data := IntToBytes(i)
+	return md5.Sum(data)
+}
+
+//整形转换成字节
+func IntToBytes(n int) []byte {
+	x := int32(n)
+	bytesBuffer := bytes.NewBuffer([]byte{})
+	_ = binary.Write(bytesBuffer, binary.BigEndian, x)
+	return bytesBuffer.Bytes()
+}
+
+//字节转换成整形
+func BytesToInt(b []byte) int {
+	bytesBuffer := bytes.NewBuffer(b)
+
+	var x int32
+	binary.Read(bytesBuffer, binary.BigEndian, &x)
+
+	return int(x)
 }
