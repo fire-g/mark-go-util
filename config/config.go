a8ab5ca641011b49742199b10b32f8f080883ee6
fire-g <18296517259@163.com>
2021-01-04 19:47:34 +0800

    add config func

---
+++ config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strings"
+)
+
+var (
+	ProjectConfig *map[string]string
+	Path          string
+)
+
+//读取key=value类型的配置文件
+func InitConfig() map[string]string {
+	config := make(map[string]string)
+
+	f, err := os.Open(Path)
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	r := bufio.NewReader(f)
+	for {
+		b, _, err := r.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+		s := strings.TrimSpace(string(b))
+		index := strings.Index(s, "=")
+		if index < 0 {
+			continue
+		}
+		key := strings.TrimSpace(s[:index])
+		if len(key) == 0 {
+			continue
+		}
+		value := strings.TrimSpace(s[index+1:])
+		if len(value) == 0 {
+			continue
+		}
+		config[key] = value
+	}
+	ProjectConfig = &config
+	return config
+}
